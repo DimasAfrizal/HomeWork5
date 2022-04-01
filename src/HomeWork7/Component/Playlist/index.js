6f6735e593d61db4becc64f4e6cc28177e598329
DimasAfrizal <96959299+DimasAfrizal@users.noreply.github.com>
2022-04-01 16:11:25 +0700

    HomeWork8

--- src/HomeWork7/Component/Playlist/index.js
+++ src/HomeWork7/Component/Playlist/index.js
@@ -1,11 +1,34 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Tracks from "../../TrackData";
 
 
 const Playlist = ({data}) => {
+
+    const [selectedTrack, setSelectedTrack] = useState([]);
+    const [combinedTracks, setCombinedTracks] = useState([]);
+
+    const handleSelectedTrack=(track)=>{
+        const alreadySelected = selectedTrack.find((v) => v.id === track.id);
+        if(alreadySelected){
+            setSelectedTrack(selectedTrack.filter((v) => v.id !== track.id));
+        }
+        else{
+            setSelectedTrack([...selectedTrack, track]);
+        }
+        console.log(selectedTrack);
+    }
+
+    useEffect(() => {
+        const combinedTracksWithSelectedTrack = data.map((track) => ({
+          ...track,
+          isSelected: selectedTrack.find((v) => v.id === track.id),
+        }));
+        setCombinedTracks(combinedTracksWithSelectedTrack);
+      }, [selectedTrack, data]);
+
     const Loop = data.map((item) => {
         return (
-            <Tracks cover={item.album.images[1].url} judul={item.name} artis={item.artists[0].name}/>
+            <Tracks  key={item.id} track={item} onSelectTrack={handleSelectedTrack}/>
         )
     })
     return(
